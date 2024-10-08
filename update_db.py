84bcc0b931a9d6b2aa1fe230fda925675834b070
Matthias Hericks <hericks@users.noreply.github.com>
2024-10-08 14:42:51 +0200

    Use new client class.

--- update_db.py
+++ update_db.py
@@ -1,108 +1,32 @@
-import json
+import os
 from datetime import datetime
-from typing import Optional
 
-import duckdb
 import polars as pl
-import requests
-from pydantic import BaseModel
-from requests.adapters import HTTPAdapter, Retry
-from tqdm import tqdm
 
-DB_NAME = "questions.db"
-TABLE_NAME = "questions"
+from client import StackoverflowQuestionsClient
+
+OUTPUT_PATH = "questions.parquet"
 TAG = "python-polars"
 
 
-class Question(BaseModel):
-    question_id: int
-
-    title: str
-    tags: list[str]
-    link: str
-    body_markdown: str
-
-    score: int
-    view_count: int
-
-    is_answered: bool
-    accepted_answer_id: Optional[int] = None
-    answer_count: int
-
-    creation_date: datetime
-    last_activity_date: datetime
-    last_edit_date: Optional[datetime] = None
-    protected_date: Optional[datetime] = None
-    closed_date: Optional[datetime] = None
-
-    crawl_date: datetime
-
-
-def requests_retry_session(
-    retries=10,
-    backoff_factor=2,
-    status_forcelist=(400,),
-):
-    session = requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount("https://", adapter)
-    return session
-
-
-def get_questions(tag: str = "python-polars") -> list[Question]:
-    session = requests_retry_session()
-    crawl_timestamp = datetime.now()
-    all_questions, page, has_more = [], 1, True
-    with tqdm() as pbar:
-        while has_more:
-            response = session.get(
-                url="https://api.stackexchange.com/2.3/questions",
-                params={
-                    "site": "stackoverflow",
-                    "tagged": tag,
-                    "page": page,
-                    "pagesize": 100,
-                    "filter": "!nNPvSNP4(R",
-                },
-            )
-            response.raise_for_status()
-            response_dict = json.loads(response.text)
-            questions = [
-                Question.model_validate(item | {"crawl_date": crawl_timestamp})
-                for item in response_dict["items"]
-            ]
-            all_questions.extend(questions)
-            page += 1
-            has_more = response_dict["has_more"]
-            pbar.update(1)
-    return all_questions
-
-
 if __name__ == "__main__":
+    client = StackoverflowQuestionsClient()
+
     print("Fetching questions...")
-    questions = get_questions(tag=TAG)
+    questions = client.get_all_questions(tag=TAG)
     df_questions = pl.DataFrame(questions)
+
+    crawl_date = datetime.now()
+    df_questions = df_questions.with_columns(pl.lit(crawl_date).alias("crawl_date"))
     print(f"Done fetching {df_questions.height} questions.")
 
-    db = duckdb.connect(DB_NAME)
-    if TABLE_NAME in db.sql("SHOW tables;").pl().to_series():
-        print(
-            f"Table '{TABLE_NAME}' in database '{DB_NAME}' already exists. Concatenating dataframe..."
-        )
-        db.sql(f"INSERT INTO questions (SELECT * FROM df_questions);")
-    else:
-        db = duckdb.connect(DB_NAME)
-        print(
-            f"Table '{TABLE_NAME}' in database '{DB_NAME}' does not exist yet. Creating from dataframe..."
-        )
-        db.sql("CREATE TABLE questions AS (SELECT * FROM df_questions);")
+    if os.path.exists(OUTPUT_PATH):
+        print(f"Storage '{OUTPUT_PATH}' exists. Loading questions...")
+        df_storage = pl.read_parquet(OUTPUT_PATH)
+        print(f"Done loading {df_storage.height} questions. Concatenating...")
+        df_questions = pl.concat([df_storage, df_questions])
+        print(f"Done concatenating dataframes.")
 
-    print("Flushing...")
-    db.sql("CHECKPOINT;")
+    print(f"Writing questions to storage '{OUTPUT_PATH}'...")
+    df_questions.write_parquet(OUTPUT_PATH)
+    print(f"Done writing questions.")
